d3a685b166e0a27575d55f35fb60f1b8ddbc8310
SaloniP <salonipawar3@gmail.com>
2023-10-20 22:39:08 -0400

    Add project files.

---
+++ HTTP5112-Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HTTP5112_Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        // GET: J1
+        /// <summary>
+        /// Calculates the total calorie count for a meal based on menu item selections.
+        /// </summary>
+        /// <param name="burger">The index of the chosen burger item.</param>
+        /// <param name="drink">The index of the chosen drink item.</param>
+        /// <param name="side">The index of the chosen side order item.</param>
+        /// <param name="dessert">The index of the chosen dessert item.</param>
+        /// <returns>The total calorie count for the selected meal.</returns>
+
+
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        [HttpGet]
+        public string GetMenu(int burger, int drink, int side, int dessert)
+        {
+            // Define the calorie values for menu items
+            int[] burgerCalories = { 461, 431, 420, 0 };
+            int[] drinkCalories = { 130, 160, 118, 0 };
+            int[] sideCalories = { 100, 57, 70, 0 };
+            int[] dessertCalories = { 167, 266, 75, 0 };
+
+            if (burger < 1 || burger > 4 || drink < 1 || drink > 4 || side < 1 || side > 4 || dessert < 1 || dessert > 4)
+            {
+                return "Invalid menu item selection";
+            }
+
+            // Calculate the total calories
+            int totalCalories = burgerCalories[burger - 1] + drinkCalories[drink - 1] + sideCalories[side - 1] + dessertCalories[dessert - 1];
+
+            return "Your total calorie count is " + totalCalories;
+        }
+    }
+}
---
+++ HTTP5112-Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HTTP5112_Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        // GET: J2
+        /// <summary>
+        /// Calculates the number of ways to roll a sum of 10 with two dice.
+        /// </summary>
+        /// <param name="m">The number of sides on the first die.</param>
+        /// <param name="n">The number of sides on the second die.</param>
+        /// <returns>The total number of ways to roll a sum of 10 with the given dice.</returns>
+        
+
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        [HttpGet]
+        public string GetDiceGame(int m, int n)
+        {
+            int count = 0;
+
+            for (int D1 = 1; D1 <= m; D1++)
+            {
+                for (int D2 = 1; D2 <= n; D2++)
+                {
+                    if (D1 + D2 == 10)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return "There are " + count + " total ways to get the sum 10.";
+              
+        }
+    }
+}
